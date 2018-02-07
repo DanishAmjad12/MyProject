7ac4ed98deb181e55b00e1a20ec90a5ab31e0e84
Danish Amjad <danishamjad@hotmail.co.uk>
2018-02-07 09:33:12 +0500

    Initial commit

---
+++ app/src/main/java/photomax/com/application/MainActivity.kt
@@ -0,0 +1,12 @@
+package photomax.com.application
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.screen_one)
+    }
+}
